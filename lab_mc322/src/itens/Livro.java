443ff80c8114ad0e4f3a5e1f65e54a2a183b4167
José Robison <Jos Robison@LAPTOP-8NNVIIDM>
2023-08-19 12:22:00 -0300

    tudo

---
+++ lab_mc322/src/itens/Livro.java
@@ -0,0 +1,49 @@
+package itens;
+
+public class Livro {
+	//Atributos
+	private String isbn; 
+	private String titulo;
+	private String autor;
+	private int dataPublicacao; 
+	
+	//Construtor
+	public Livro(String isbn, String titulo, String autor, int dataPublicacao) {
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.dataPublicacao = dataPublicacao;
+	}
+	
+	//Getters e setters
+	public String getisbn() {
+		return isbn;
+	}
+	public void setisbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String gettitulo() {
+		return titulo;
+	}
+	public void settitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getautor() {
+		return autor;
+	}
+	public void setautor(String autor) {
+		this.autor = autor;
+	}
+	
+	public int getdataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setdataPublicacao(int dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	
+	
+
+}
